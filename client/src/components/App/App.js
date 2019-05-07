c9239f74d0d8f0e15e3161d2f43830bea30a853f
Dylan Finn <dylanfinn89@gmail.com>
2019-05-07 01:49:18 -0700

    refactor, React and Scrapper

---
+++ client/src/components/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
